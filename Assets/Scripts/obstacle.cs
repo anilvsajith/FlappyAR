b25c6c9a22b6103e2c92052293583eceaa13ecbf
Aadithya <aadithyamd@gmail.com>
2016-06-15 15:13:18 +0530

    Fixed Obstacle oscillation

--- Assets/Scripts/obstacle.cs
+++ Assets/Scripts/obstacle.cs
@@ -10,29 +10,25 @@ public class obstacle : MonoBehaviour {
 	public float m_limit;
 	public float m_Speed,u_speed=2f,limit=1f;
 	float m_randpos;
-	float m_randsize;
+	float spacing;
 	float pos;
-	bool start= false,isUpward=true;
+	bool start= false,isUpward=true,activated=false,topMoving;
 	Vector3 tempTop;
+	float minGap;
 
 	// Use this for initialization
 	void Start () {
 
+
+		topMoving = false;
+		
+		activated = false;
+		minGap = 97f;
 		//Get Bottom and Top Obstacles
 		m_Bottom = transform.FindChild ("Bottom");
 		m_Top = transform.FindChild ("Top");
-		pos = m_Bottom.transform.position.y;
+		pos = transform.localPosition.y;
 
-		//Give Random Position to the obstacles
-		m_randpos = pos + 1 + Random.Range (-1.5f, 1.5f);
-		m_randsize = m_randpos + 20.75f+ Random.Range (-0.75f, 0.75f);
-		m_Bottom.transform.position = new Vector3(transform.position.x,m_randpos,m_Bottom.transform.position.z);
-
-		tempTop.x = transform.position.x;
-		tempTop.z = m_Bottom.transform.position.z;
-		tempTop.y = m_randsize;
-		m_Top.transform.position = tempTop;
-			//new Vector3 (transform.position.x, m_randsize, m_Bottom.transform.position.z);
 	}
 
 	//Function to Reset the obstacles on Restart 
@@ -41,15 +37,40 @@ public class obstacle : MonoBehaviour {
 		start = false;
 		m_Top.gameObject.SetActive (false);
 		m_Bottom.gameObject.SetActive (false);
-		transform.position = RestartPosition;
+		transform.localPosition = RestartPosition;
+		setRandPos ();
+		// top won't move for the first 4 obstacles
+		topMoving = false;
+		activated = false;
+	}
+
+	void setRandPos (){
+		float a = Random.Range (0f, 5f);
+		if (a > 4.3f||a<0.4f)
+			topMoving = true;
+		else
+			topMoving = false;
+		//Give Random Position to the obstacles
+		m_randpos = pos + Random.Range (-1f, 3.5f);
+		spacing = minGap + Random.Range (0, 10f);
+
+		Debug.Log (spacing);
+
+		transform.localPosition = new Vector3(transform.localPosition.x,m_randpos,transform.localPosition.z);
+
+		tempTop.x = 0;
+		tempTop.z = 0;
+		tempTop.y = spacing;
+		m_Top.transform.localPosition = tempTop;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		
-		if (transform.position.x <= 28) {
+		if (transform.localPosition.x <= 28&&activated == false) {
 			m_Top.gameObject.SetActive (true);
 			m_Bottom.gameObject.SetActive (true);
+			activated = true;
 		}
 
 		//Start the Obstacle Movement on First Click
@@ -60,23 +81,34 @@ public class obstacle : MonoBehaviour {
 		//Movement of Obstacle
 		if(start){
 
-			if (transform.position.x > m_limit) {
+			if (transform.localPosition.x > m_limit) {
 				transform.Translate (Vector3.left * Time.deltaTime* m_Speed);
 			} else {
 				//Reset Position and give random size on reaching limit
-				m_randpos = pos + 1 + Random.Range (-1.5f, 1.5f);
-				m_randsize = m_randpos + 20.75f+ Random.Range (-0.75f, 0.75f);
-				m_Bottom.transform.position = new Vector3(transform.position.x,m_randpos,m_Bottom.transform.position.z);
-
-				tempTop.x = transform.position.x;
-				tempTop.z = m_Bottom.transform.position.z;
-				tempTop.y = m_randsize;
-				m_Top.transform.position = tempTop;
-
-				m_Top.gameObject.SetActive (false);
-				m_Bottom.gameObject.SetActive (false);
-				transform.position = ResetPosition;
+				transform.localPosition = ResetPosition;
+				setRandPos();
 			}
+
+		}
+
+		if (GameManager.paused == false&&topMoving) {
+			
+			if (isUpward) {
+				if (tempTop.y < spacing + limit)
+					tempTop.y += u_speed * Time.deltaTime;
+				else
+					isUpward = false;	
+			} else {
+				tempTop.y -= u_speed * Time.deltaTime;
+				if (tempTop.y > spacing)
+					;
+				else {
+					isUpward = true;
+					tempTop.y = spacing;
+				}
+			}
+
+			m_Top.transform.localPosition = tempTop;
 
 		}
 	
